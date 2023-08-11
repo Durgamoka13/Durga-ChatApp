900b47e1ea3d800586fee0b3834eb42ed25ebb5f
Durgamoka13 <133496310+Durgamoka13@users.noreply.github.com>
2023-08-11 22:13:02 +0530

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAhbdiK9Wq080FbT15wv8hnL-8VmHHcJGM",
+  authDomain: "durga-chatapp.firebaseapp.com",
+  projectId: "durga-chatapp",
+  storageBucket: "durga-chatapp.appspot.com",
+  messagingSenderId: "306185068559",
+  appId: "1:306185068559:web:96d6443ced484ce71a437a"
+};
+
+
+export const app = initializeApp(firebaseConfig);
